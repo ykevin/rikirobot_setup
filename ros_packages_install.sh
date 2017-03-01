6d44678122df5ecbe2cb7f15f5c5230fe7aa2223
ykevin <15018787835@163.com>
2017-03-01 22:32:13 +0800

    add source code robot

--- ros_packages_install.sh
+++ ros_packages_install.sh
@@ -6,7 +6,14 @@ sudo apt-get install -y ros-kinetic-xv-11-laser-driver
 sudo apt-get install -y ros-kinetic-gmapping
 sudo apt-get install -y ros-kinetic-navigation
 sudo apt-get install -y ros-kinetic-map-server
+
 cd ~/catkin_ws/src
+
+git clone https://github.com/ykevin/rikirobot.git
+git clone https://github.com/ykevin/rplidar_ros.git
+git clone https://github.com/ykevin/riki_visualize.git
+git clone https://github.com/ykevin/riki_msgs.git
+git clone https://github.com/ykevin/ros_arduino.git
 
 
 echo "ROS Packages Installed Successfully"
