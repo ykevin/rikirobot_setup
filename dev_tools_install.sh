2b2202b961656867d8e6a7d483e20b52f53b7414
ykevin <15018787835@163.com>
2017-05-15 16:57:48 +0800

    add astra xtion driver

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -12,9 +12,4 @@ sudo rm -rf ~/.platformio/
 sudo cp 49-teensy.rules /etc/udev/rules.d/
 
 echo "Installation Done"
-#for raspberry pi
-#mkdir /tmp/mpc && cd /tmp/mpc
-#wget http://ftp.de.debian.org/debian/pool/main/m/mpclib/libmpc2_0.9-4+b1_armhf.deb
-#sudo dpkg -i libmpc2_0.9-4+b1_armhf.deb
-#sudo ldconfig
 
--- ros_packages_install.sh
+++ ros_packages_install.sh
@@ -1,12 +1,13 @@
 #sudo apt-get update
+sudo apt-get install -y libv4l-dev 
 sudo apt-get install -y ros-kinetic-roslint
 sudo apt-get install -y ros-kinetic-rosserial
 sudo apt-get install -y ros-kinetic-imu-filter-madgwick
-sudo apt-get install -y ros-kinetic-xv-11-laser-driver
 sudo apt-get install -y ros-kinetic-gmapping
 sudo apt-get install -y ros-kinetic-navigation
 sudo apt-get install -y ros-kinetic-map-server
 sudo apt-get install -y ros-kinetic-rgbd-launch
+
 
 cd ~/catkin_ws/src
 
@@ -16,6 +17,14 @@ git clone https://github.com/ykevin/riki_visualize.git
 git clone https://github.com/ykevin/riki_msgs.git
 git clone https://github.com/ykevin/ros_arduino.git
 git clone https://github.com/ykevin/riki_pid.git
+
+echo "add camera ros packages!!"
+git clone https://github.com/ktossell/camera_umd.git
+
+echo "add Astra xtion live"
+echo "Astra xtion driver please: https://orbbec3d.com/develop/"
+git clone https://github.com/orbbec/ros_astra_launch.git
+git clone https://github.com/orbbec/ros_astra_camera.git
 
 
 echo "ROS Packages Installed Successfully"
