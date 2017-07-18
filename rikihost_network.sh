44b3eb973d22628e4ffb0af588bf9d0b141127a9
ykevin <15018787835@163.com>
2017-07-18 23:00:48 +0800

    modfiy host network bash

--- rikihost_network.sh
+++ rikihost_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "export ROS_IP=`hostname -I`" >> ~/.bashrc
-echo "export ROS_HOSTNAME=`hostname -I`" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://`hostname -I`:11311" >> ~/.bashrc
+echo "export ROS_IP=\`hostname -I\`" >> ~/.bashrc
+echo "export ROS_HOSTNAME=\`hostname -I\`" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://\`hostname -I\`:11311" >> ~/.bashrc
 
 echo "Network setup done"
