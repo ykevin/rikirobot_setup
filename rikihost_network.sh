f7d46bde8bc3038ca9a5f44efa241f6ed1a31285
ykevin <15018787835@163.comn>
2018-05-21 09:35:52 +0800

    add network

--- rikihost_network.sh
+++ rikihost_network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "export ROS_IP=\`hostname -I\`" >> ~/.bashrc
-echo "export ROS_HOSTNAME=\`hostname -I\`" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://\`hostname -I\`:11311" >> ~/.bashrc
+export ROS_IP=`hostname -I | awk '{print $1}'`
+export ROS_HOSTNAME=`hostname -I | awk '{print $1}'`
+export ROS_MASTER_URI=http://`hostname -I | awk '{print $1}'`:11311
 echo "export RIKIBASE=2wd" >> ~/.bashrc
 echo "export RIKILIDAR=rplidar" >> ~/.bashrc
 
