bc4fbbb0631575cd176a7a4f371974b9dfb856f1
ykevin <15018787835@163.com>
2018-07-13 18:40:53 +0800

    add ros bash config

--- rikihost_network.sh
+++ rikihost_network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export ROS_IP=`hostname -I | awk '{print $1}'`
-export ROS_HOSTNAME=`hostname -I | awk '{print $1}'`
-export ROS_MASTER_URI=http://`hostname -I | awk '{print $1}'`:11311
+echo "export ROS_IP=\`hostname -I | awk '{print $1}'\`" >> ~/.bashrc
+echo "export ROS_HOSTNAME=\`hostname -I | awk '{print $1}'\`" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://\`hostname -I | awk '{print $1}'\`:11311" >> ~/.bashrc
 echo "export RIKIBASE=2wd" >> ~/.bashrc
 echo "export RIKILIDAR=rplidar" >> ~/.bashrc
 
