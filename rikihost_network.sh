f0edcc46ba7df818f3a045fd269398f11b347a16
ykevin <15018787835@163.com>
2018-09-27 21:39:19 +0800

    modfiy install rikirobot bash

---
+++ rikihost_network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "export ROS_IP=\`hostname -I | awk '{print \$1}'\`" >> ~/.bashrc
+echo "export ROS_HOSTNAME=\`hostname -I | awk '{print \$1}'\`" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://\`hostname -I | awk '{print \$1}'\`:11311" >> ~/.bashrc
+echo "export RIKIBASE=2wd" >> ~/.bashrc
+echo "export RIKILIDAR=rplidar" >> ~/.bashrc
+
+echo "Network setup done"
