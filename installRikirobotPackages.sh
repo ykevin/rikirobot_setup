6e4d6b30c02c1eafa7275bfa92b266b8595605a2
ykevin <15018787835@163.com>
2020-07-18 06:50:20 +0800

    modfiy some install

--- installRikirobotPackages.sh
+++ installRikirobotPackages.sh
@@ -2,16 +2,16 @@ sudo cp 58-riki.rules /etc/udev/rules.d/
 sudo cp 558-orbbec-usb.rules /etc/udev/rules.d/
 sudo apt-get update
 ver=$(rosversion -d)
-sudo apt-get install -y libusb-dev libspnav-dev  libbluetooth-dev libcwiid-dev
-sudo apt-get install -y git vim
-sudo pip install pybluez
+#sudo apt-get install -y libusb-dev libspnav-dev  libbluetooth-dev libcwiid-dev
+#sudo apt-get install -y git vim
+#sudo pip install pybluez
 sudo apt-get install -y libv4l-dev 
 sudo apt-get install -y build-essential
 sudo apt-get install -y chrony
-sudo apt-get install -y libbullet-dev 
+#sudo apt-get install -y libbullet-dev 
 sudo apt-get install -y libudev-dev
 sudo apt-get install -y libusb-dev
-sudo apt-get install -y mini-httpd
+#sudo apt-get install -y mini-httpd
 sudo apt-get install -y ros-$ver\-roslint
 sudo apt-get install -y ros-$ver\-rosserial
 sudo apt-get install -y ros-$ver\-gmapping
@@ -29,7 +29,7 @@ sudo apt-get install -y ros-$ver\-rosbridge-suite
 sudo apt-get install -y ros-$ver\-robot-pose-publisher
 sudo apt-get install -y ros-$ver\-tf2-web-republisher
 sudo apt-get install -y ros-$ver\-web-video-server
-sudo apt-get install ros-kinetic-vision-msgs
+sudo apt-get install -y ros-$ver\-vision-msgs
 
 
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
