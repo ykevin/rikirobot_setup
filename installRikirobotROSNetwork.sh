d2959563c8a7a37806f9d70a9431b84c639eee5a
ykevin <15018787835@163.com>
2020-07-20 14:26:34 +0800

    add serial

--- installRikirobotROSNetwork.sh
+++ installRikirobotROSNetwork.sh
@@ -5,6 +5,6 @@ echo "export ROS_MASTER_URI=http://\`hostname -I | awk '{print \$1}'\`:11311" >>
 echo "export RIKIBASE=2wd" >> ~/.bashrc
 echo "export RIKILIDAR=rplidar" >> ~/.bashrc
 echo "export RIKILIDAR=rplidar" >> ~/.bashrc
-echo "export RIKI_SERIAL="Adsgdsgad"" >> ~/.bashrc
+echo "export RIKI_SERIAL=AEEFGABES63K6M2JXWMTA4VUBU======" >> ~/.bashrc
 
 echo "Network setup done"
